539a8766c0baa9d7c0c7de41ee9921037e0a7406
Hoang Pham <hoanglk12@gmail.com>
2021-08-24 19:54:02 +0700

    Topic_17_Wait_Part_III_IV (Static and Explicit Wait)

---
+++ selenium/selenium/Topic_17_Wait_Part_III_Static_Wait.java
@@ -0,0 +1,228 @@
+package selenium;
+
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_17_Wait_Part_III_Static_Wait {
+
+	String projectPath = System.getProperty("user.dir");
+	WebDriver driver;
+	JavascriptExecutor jsExecutor;
+	Actions action;
+	WebDriverWait explicitWait;
+	String autoITPath = projectPath + "\\autoIT\\authen_firefox.exe";
+	By confirmEmailTextbox = By.xpath("//input[@name='reg_email_confirmation__']");
+	By startButton = By.cssSelector("#start>button");
+	By loadingIcon = By.cssSelector(".example #loading");
+	By helloWorldText = By.cssSelector("#finish>h4");
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		jsExecutor = (JavascriptExecutor) driver;
+		//explicitWait = new WebDriverWait(driver, 15);
+		action = new Actions(driver);
+		
+	}
+
+	@Test
+	public void TC_01_Static_Wait_Less_Than() {
+	
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		clickElement(startButton);
+		
+		sleepInSecond(3);
+		
+		Assert.assertTrue(driver.findElement(helloWorldText).isDisplayed());
+		Assert.assertEquals(driver.findElement(helloWorldText).getText(), "Hello World!");
+	}
+
+	@Test
+	public void TC_02_Static_Wait_Equal() {
+		
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		clickElement(startButton);
+		
+		sleepInSecond(5);
+		
+		Assert.assertTrue(driver.findElement(helloWorldText).isDisplayed());
+		Assert.assertEquals(driver.findElement(helloWorldText).getText(), "Hello World!");
+	}
+
+	@Test
+	public void TC_03_Static_Wait_Greater_Than() {
+	
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		clickElement(startButton);
+		
+		sleepInSecond(10);
+		
+		Assert.assertTrue(driver.findElement(helloWorldText).isDisplayed());
+		Assert.assertEquals(driver.findElement(helloWorldText).getText(), "Hello World!");
+	}
+	
+	
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	
+	public Object executeForBrowser(String javaScript) {
+		return jsExecutor.executeScript(javaScript);
+	}
+
+	public String getInnerText() {
+		return (String) jsExecutor.executeScript("return document.documentElement.innerText;");
+	}
+
+	public boolean isExpectedTextInInnerText(String textExpected) {
+		String textActual = (String) jsExecutor
+				.executeScript("return document.documentElement.innerText.match('" + textExpected + "')[0];");
+		return textActual.equals(textExpected);
+	}
+
+	public void scrollToBottomPage() {
+		jsExecutor.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+	}
+
+	public void navigateToUrlByJS(String url) {
+		jsExecutor.executeScript("window.location = '" + url + "'");
+	}
+
+	public void hightlightElement(String locator) {
+		WebElement element = getElement(locator);
+		String originalStyle = element.getAttribute("style");
+		jsExecutor.executeScript("arguments[0].setAttribute(arguments[1], arguments[2])", element, "style",
+				"border: 2px solid red; border-style: dashed;");
+		sleepInSecond(1);
+		jsExecutor.executeScript("arguments[0].setAttribute(arguments[1], arguments[2])", element, "style",
+				originalStyle);
+	}
+
+	public void clickToElementByJS(String locator) {
+		jsExecutor.executeScript("arguments[0].click();", getElement(locator));
+	}
+
+	public void scrollToElement(String locator) {
+		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", getElement(locator));
+	}
+
+	public void sendkeyToElementByJS(String locator, String value) {
+		jsExecutor.executeScript("arguments[0].setAttribute('value', '" + value + "')", getElement(locator));
+	}
+
+	public void removeAttributeInDOM(String locator, String attributeRemove) {
+		jsExecutor.executeScript("arguments[0].removeAttribute('" + attributeRemove + "');", getElement(locator));
+	}
+
+	public String getElementValidationMessage(String locator) {
+		return (String) jsExecutor.executeScript("return arguments[0].validationMessage;", getElement(locator));
+	}
+
+	public boolean isImageLoaded(String locator) {
+		boolean status = (boolean) jsExecutor.executeScript(
+				"return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0",
+				getElement(locator));
+		return status;
+	}
+
+	public WebElement getElement(String locator) {
+		return driver.findElement(By.xpath(locator));
+	}
+
+
+	public boolean isElementDisplayed(By by) {
+		if (driver.findElement(by).isDisplayed()) {
+			System.out.println(by + " is displayed");
+			return true;
+		} else {
+			System.out.println(by + " is not displayed");
+			return false;
+		}
+	}
+
+	public boolean isElementEnabled(By by) {
+		if (driver.findElement(by).isEnabled()) {
+			System.out.println(by + " is enabled");
+			return true;
+		} else {
+			System.out.println(by + " is disabled");
+			return false;
+		}
+	}
+
+	public boolean isElementSelected(By by) {
+		if (driver.findElement(by).isSelected()) {
+			System.out.println(by + " is selected");
+			return true;
+		} else {
+			System.out.println(by + " is de-selected");
+			return false;
+		}
+	}
+
+	public void sendKeysToElement(By by, String key) {
+		driver.findElement(by).clear();
+		driver.findElement(by).sendKeys(key);
+	}
+
+	public void clickElement(By by) {
+		driver.findElement(by).click();
+	}
+
+	public void clickByJs(By by) {
+		jsExecutor.executeScript("arguments[0].click();", driver.findElement(by));
+	}
+	
+	public void removeAttributeByJs(By by) {
+		jsExecutor.executeScript("arguments[0].removeAttribute('disabled')", driver.findElement(by));
+	}
+	
+	public void checkToCheckboxOrRadioButton(By by) {
+		WebElement checkbox = driver.findElement(by);
+		if(!checkbox.isSelected()) {
+			checkbox.click();
+		}
+	}
+	
+	public void uncheckToCheckbox(By by) {
+		WebElement checkbox = driver.findElement(by);
+		if(checkbox.isSelected()) {
+			checkbox.click();
+		}
+	}
+	
+	public void accessSplitandJoinUrl(String url, String username, String password) {
+		String[] hrefUrl = url.split("//");
+		url = hrefUrl[0] + "//" + username + ":" + password + "@" + hrefUrl[1];
+		driver.get(url);
+	}
+	
+	public String generateEmail() {
+		Random rand = new Random();
+		return rand.nextInt(9999) + "@mail.com";
+	}
+
+	public void sleepInSecond(long timeOutSecond) {
+		try {
+			Thread.sleep(timeOutSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
